4c26fb9f30faac25884d90fcbef94e414da46f8f
Glen Bradley <glenbolake@gmail.com>
2024-12-04 10:11:19 -0500

    Add graph to day 2 animation

--- animations/day02.py
+++ animations/day02.py
@@ -1,11 +1,75 @@
+from typing import Literal
+
 import pygame
 from pygame import K_ESCAPE, Vector2
 from pygame.color import THECOLORS
 
 TEXT_SIZE = 24
 FPS = 60
+TICK_SPEED = 300
 
 DEFAULT_FONT: pygame.font.Font = None  # noqa
+
+
+class Report:
+    GRAPH_WIDTH = 300
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def check_valid(self, part: Literal[1, 2]) -> tuple[bool, int | None]:
+        def check_levels(levels):
+            diffs = {a - b for a, b in zip(self.nums, self.nums[1:])}
+            if diffs <= {1, 2, 3} or diffs <= {-1, -2, -3}:
+                return True
+
+        if check_levels(self.nums):
+            return True, None
+        elif part == 2:
+            for i in range(len(self.nums)):
+                if check_levels(self.nums[:i] + self.nums[i + 1:]):
+                    return True, i
+        return False, None
+
+    def render(self, surface: pygame.Surface, pos: Vector2):
+        top = pos.y
+        bottom = pos.y + TEXT_SIZE * 3
+        max_value = max(self.nums)
+        spacing = self.GRAPH_WIDTH / len(self.nums)
+
+        def value_to_coord(value):
+            # 0 should map to "bottom" and the max value should map to "top"
+            # y = ax+b
+            a = (top - bottom) / max_value
+            b = bottom
+            return a * value + b
+
+        points = [
+            Vector2(pos.x + x * spacing, value_to_coord(y))
+            for x, y in enumerate(self.nums)
+        ]
+        left = points[0].x
+        right = points[-1].x
+        points.extend([
+            Vector2(right, bottom),
+            Vector2(left, bottom)
+        ])
+        pygame.draw.polygon(surface, THECOLORS['cadetblue'], points)
+
+        # Get the trend in a lazy way
+        ups = downs = 0
+        for a,b in zip(self.nums, self.nums[1:]):
+            if a<b:
+                ups += 1
+            elif a>b:
+                downs += 1
+        trend = 1 if ups >= downs else -1
+        for left_point, left_value, right_point, right_value in zip(points, self.nums, points[1:], self.nums[1:]):
+            slope = right_value - left_value
+            color = THECOLORS['black'] if 1 <= abs(slope) <= 3 else THECOLORS['red']
+            if slope * trend < 0:
+                color = THECOLORS['red']
+            pygame.draw.line(surface, color, left_point, right_point, width=2)
 
 
 class ScreenInt:
@@ -67,15 +131,16 @@ def main(filename):
     DEFAULT_FONT = pygame.font.SysFont('Verdana', TEXT_SIZE)
     window = pygame.display.set_mode((1024, 768))
 
-    report_pos = Vector2(TEXT_SIZE, TEXT_SIZE)
-    diff_pos = Vector2(TEXT_SIZE, TEXT_SIZE * 2.5)
-    sum_pos = Vector2(TEXT_SIZE, TEXT_SIZE * 5)
+    report_pos = Vector2(TEXT_SIZE, TEXT_SIZE*5)
+    diff_pos = Vector2(TEXT_SIZE, TEXT_SIZE * 6.5)
+    sum_pos = Vector2(TEXT_SIZE, TEXT_SIZE * 9)
 
     pygame.display.set_caption('Day 2: Red-Nosed Reports')
     pygame.display.set_icon(pygame.image.load('assets/aoc_favicon.png'))
 
     clock = pygame.time.Clock()
 
+    rendered_report: Report
     report: list[ScreenInt] = []
     diffs: list[ScreenInt] = []
     total = ScreenInt(0, sum_pos)
@@ -87,6 +152,7 @@ def main(filename):
         yield total
 
     def set_current_report(new_report):
+        nonlocal rendered_report
         space_width, _ = DEFAULT_FONT.render(' ' * 5, True, THECOLORS['black']).get_size()
         current_x, current_y = report_pos
 
@@ -96,6 +162,7 @@ def main(filename):
             current_x += item.rect.width + space_width
             return item
 
+        rendered_report = Report(new_report)
         return [add_item(n) for n in new_report]
 
     def calc_diffs():
@@ -126,7 +193,6 @@ def main(filename):
     add_to_total = pygame.USEREVENT + 4
     reports = get_lines()
 
-    tick_speed = 100
     running = True
 
     def process_events():
@@ -138,9 +204,9 @@ def main(filename):
                 # print('next report')
                 report = set_current_report(next(reports))
                 diffs.clear()
-                pygame.time.set_timer(make_diffs, tick_speed, 1)
-                pygame.time.set_timer(check_diffs, 2*tick_speed, 1)
-                pygame.time.set_timer(add_to_total, 3*tick_speed, 1)
+                pygame.time.set_timer(make_diffs, TICK_SPEED, 1)
+                pygame.time.set_timer(check_diffs, 2 * TICK_SPEED, 1)
+                pygame.time.set_timer(add_to_total, 3 * TICK_SPEED, 1)
             if event.type == make_diffs:
                 # print('diffs')
                 diffs = calc_diffs()
@@ -153,12 +219,13 @@ def main(filename):
 
     def render():
         window.fill(THECOLORS['aliceblue'])
+        rendered_report.render(window, Vector2(TEXT_SIZE, TEXT_SIZE))
         for text in rendered_texts():
             text.render(window)
         pygame.display.update()
 
     pygame.event.post(pygame.event.Event(next_report))
-    pygame.time.set_timer(next_report, 5*tick_speed)
+    pygame.time.set_timer(next_report, 5 * TICK_SPEED)
     while running:
         try:
             process_events()
