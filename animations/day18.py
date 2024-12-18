b37c47a4bacae3e873c97c7773b08aa97a22da14
Glen Bradley <glen.bradley@thebridgecorp.com>
2024-12-18 15:07:28 -0500

    Day 18, part 2

--- animations/day18.py
+++ animations/day18.py
@@ -11,18 +11,19 @@ from animations import Point, Model, Animation
 
 
 @lru_cache
-def byte_sprite(tile_size):
+def byte_sprite(tile_size, highlight=False):
     surface = pygame.Surface((tile_size, tile_size), pygame.SRCALPHA)
-    corner = tile_size // 10
+    corner = max(1, tile_size // 10)
+    bg_color = (128,128,128,255) if highlight else (0,0,0,255)
     green = 0, 255, 0, 255
-    pygame.draw.rect(surface, (0, 0, 0, 255), (0, 0, tile_size, tile_size), border_radius=corner)
-    pygame.draw.rect(surface, green, (0, 0, tile_size, tile_size), width=5, border_radius=corner)
+    pygame.draw.rect(surface, bg_color, (0, 0, tile_size, tile_size), border_radius=corner)
+    pygame.draw.rect(surface, green, (0, 0, tile_size, tile_size), width=1, border_radius=corner)
     pygame.draw.circle(surface, green, (tile_size * .3, tile_size * .4), tile_size / 15)
     pygame.draw.circle(surface, green, (tile_size * .7, tile_size * .4), tile_size / 15)
-    pygame.draw.line(surface, green, (tile_size * .25, tile_size * .25), (tile_size * .35, tile_size * .3), 2)
-    pygame.draw.line(surface, green, (tile_size * .75, tile_size * .25), (tile_size * .65, tile_size * .3), 2)
+    pygame.draw.line(surface, green, (tile_size * .25, tile_size * .25), (tile_size * .35, tile_size * .3), 1)
+    pygame.draw.line(surface, green, (tile_size * .75, tile_size * .25), (tile_size * .65, tile_size * .3), 1)
     pygame.draw.arc(surface, green, (tile_size * .2, tile_size * .5, tile_size * .6, tile_size * .3), math.pi,
-                    2 * math.pi, 2)
+                    2 * math.pi, 1)
     return surface
 
 
@@ -35,35 +36,59 @@ def parse(incoming_bytes: str):
 
 
 class Grid(Model):
-    def __init__(self, input: str, size: int):
+    def __init__(self, input: str, size: int, init_read=0):
         self.all_points = parse(input)
         self.corruption: set[Point] = set()
+        self.last_added: Point = None
         self.size = size
-        self.tick_rate = 2  # Tick every n frames
+        self.tick_rate = 3  # Tick every n frames
         self.counter = cycle(range(self.tick_rate))
-        # BFS stuff
-        self.dead_ends: set[Point] = set()
-        self.step_history: dict[Point, set[Point] | None] = defaultdict(set)
-        self.search_history: dict[Point, int] = {}
-        self.queue = deque()
+        self.path: list[Point] = []
         self.goal = Point(self.size - 1, self.size - 1)
         self.finished = False
+        self.read_points(init_read)
+        self.update_path()
+        print('Part 1:', len(self.path) - 1)
 
-    def on_path(self, point):
-        if point == self.goal:
-            return True
-        if point in self.dead_ends:
-            return False
-        reached = self.step_history[point]
-        if reached is None:
-            result = False
-        elif len(reached) == 0:  # Empty set means it's probably on the queue
-            result = True
-        else:
-            result = any(self.on_path(p) for p in reached)
-        if not result:
-            self.dead_ends.add(point)
-        return result
+    def update_path(self):
+        seen: set[Point] = set()
+        queue = deque([[Point(0, 0)]])
+        while queue:
+            point, *history = queue.popleft()
+            if point in seen:
+                continue
+            seen.add(point)
+            if point == self.goal:
+                self.path = [point, *history]
+                return
+            for diff in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                new_x, new_y = new_point = point + diff
+                if 0 <= new_x < self.size and 0 <= new_y < self.size and (new_x, new_y) not in self.corruption:
+                    queue.append([new_point, point, *history])
+        # No path possible
+        self.path = self.flood_fill(Point(0, 0))
+        self.finished = True
+        print('Part 2:', self.last_added)
+
+    def flood_fill(self, start):
+        seen: set[Point] = set()
+        queue = deque([start])
+        while queue:
+            point = queue.popleft()
+            if point in seen:
+                continue
+            seen.add(point)
+            queue.extend([
+                new_point
+                for diff in [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                if (
+                        (new_point := point + diff) not in self.corruption
+                        and 0 <= new_point.x < self.size
+                        and 0 <= new_point.y < self.size
+                )
+
+            ])
+        return list(seen)
 
     def print_grid(self):
         for y in range(self.size):
@@ -71,66 +96,42 @@ class Grid(Model):
                 print('#' if (x, y) in self.corruption else '.', end='')
             print()
 
-    def tick_bfs(self):
-        if not self.finished and self.goal in self.search_history:
-            self.finished = True
-            print('Part 1:', self.search_history[self.goal])
-        if self.queue:
-            point, score = self.queue.popleft()
-            dead_end = True
-            for diff in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
-                new_x, new_y = new_point = point + diff
-                if 0 <= new_x < self.size and 0 <= new_y < self.size and (new_x, new_y) not in self.corruption:
-                    new_score = score + 1
-                    if self.search_history.get(new_point, sys.maxsize) > new_score:
-                        dead_end = False
-                        self.queue.append((new_point, new_score))
-                        self.search_history[new_point] = new_score
-                        self.step_history[point].add(new_point)
-            if dead_end:
-                self.step_history[point] = None
-
-        elif not self.search_history:
-            # We haven't even started.
-            self.search_history[Point(0, 0)] = 0
-            self.queue.append((Point(0, 0), 0))
-
     def read_points(self, n):
         for _ in range(n):
-            self.corruption.add(next(self.all_points))
+            self.last_added = next(self.all_points)
+            self.corruption.add(self.last_added)
 
     def update(self):
-        if next(self.counter) > 0:
+        if self.finished or next(self.counter) > 0:
             return
-        if not self.finished:
-            self.tick_bfs()
+        if self.path:
+            self.read_points(1)
+            self.update_path()
 
     def render(self, surface: pygame.Surface):
         tile_size = surface.get_height() // self.size
-        for point in self.search_history:
-            color = (0, 0, 255) if self.on_path(point) else (255, 0, 0)
+        for point in self.path:
+            color = (255, 0, 0) if self.finished else (0, 0, 255)
             surface.fill(color, (pygame.Vector2(point).elementwise() * tile_size, (tile_size, tile_size)))
         for i in range(self.size + 1):
-            pygame.draw.line(surface, (255, 0, 0, 25), (0, tile_size * i), (tile_size * self.size, tile_size * i), 3)
-            pygame.draw.line(surface, (255, 0, 0, 25), (tile_size * i, 0), (tile_size * i, tile_size * self.size), 3)
+            pygame.draw.line(surface, (255, 0, 0, 25), (0, tile_size * i), (tile_size * self.size, tile_size * i), 1)
+            pygame.draw.line(surface, (255, 0, 0, 25), (tile_size * i, 0), (tile_size * i, tile_size * self.size), 1)
         for byte in self.corruption:
             blit_pos = pygame.Vector2(byte).elementwise() * tile_size
-            surface.blit(byte_sprite(tile_size), blit_pos)
+            surface.blit(byte_sprite(tile_size, highlight=byte==self.last_added), blit_pos)
 
 
-def part1(filename):
+def solve(filename):
     if 'sample' in filename:
         size = 7
         num_points = 12
     else:
         size = 71
         num_points = 1024
-    grid = Grid(filename, size)
-    grid.read_points(num_points)
-    grid.print_grid()
-    Animation(caption='Day 18: RAM Run').run(grid)
+    grid = Grid(filename, size, init_read=num_points)
+    Animation(caption='Day 18: RAM Run', fps=240).run(grid)
 
 
 if __name__ == '__main__':
-    part1('../inputs/sample18.txt')
-    part1('../inputs/day18.txt')
+    solve('../inputs/sample18.txt')
+    solve('../inputs/day18.txt')
